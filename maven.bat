860b4e9114404b004fa138f8aaf949f62bc81f00
Jayparmar98 <99892507+Jayparmar98@users.noreply.github.com>
2023-06-08 10:02:00 +0530

    skip test

--- maven.bat
+++ maven.bat
@@ -9,6 +9,6 @@ cd "C:\jenkins_workspace\workspace\DeskBook-FEB\Deskbook-Backend-Java-Feb\"
 call "%JDK_PATH%\bin\javac" -version
 call "%JDK_PATH%\bin\java" -version
 
-call mvn clean install
+call mvn package -Dmaven.test.skip
 
 endlocal
