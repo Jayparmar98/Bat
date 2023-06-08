3cf03b67f94a1a2a398bbb0cf6306baee48e8e0a
Jayparmar98 <99892507+Jayparmar98@users.noreply.github.com>
2023-06-08 10:51:56 +0530

    set maven path.bat

--- maven.bat
+++ maven.bat
@@ -3,11 +3,14 @@
 setlocal
 
 set JDK_PATH=C:\Program Files\Java\jdk-17
+set M2_HOME=C:\Path\to\Your\Maven
+
+set PATH=%JDK_PATH%\bin;%M2_HOME%\bin;%PATH%
 
 cd "C:\jenkins_workspace\workspace\DeskBook-FEB\Deskbook-Backend-Java-Feb\"
 
 call "%JDK_PATH%\bin\javac" -version
-call "%JDK_PATH%\bin\java" -version
+call "%M2_HOME%\bin\mvn" -version
 
 call mvn package -Dmaven.test.skip
 
